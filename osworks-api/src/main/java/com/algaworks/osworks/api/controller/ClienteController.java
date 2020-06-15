e2d5abba0bfb1570db532ddff84a8a980beab286
pedro miquiles <miquiles.lima@hotmail.com>
2020-06-15 16:23:26 -0300

    Ajustes em desempenho

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -15,14 +15,14 @@ public class ClienteController {
 	public List<Cliente> listar() {
 		Cliente cliente1 = new Cliente();
 		cliente1.setId(1L);
-		cliente1.setNome("Pedro");
+		cliente1.setNome("Pedro Henrique");
 		cliente1.setTelefone("32252222");
 		cliente1.setEmail("pedro@gmail.com");
 		
 		Cliente cliente2 = new Cliente();
 		
 		cliente2.setId(2L);
-		cliente2.setNome("Amanda");
+		cliente2.setNome("Amanda Alves");
 		cliente2.setTelefone("32255555");
 		cliente2.setEmail("amanda@gmail.com");
 		
