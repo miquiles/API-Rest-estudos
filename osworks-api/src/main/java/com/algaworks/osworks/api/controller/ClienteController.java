0a8a953aaaf63eacc81135bd24d93245d64f16fd
pedro miquiles <miquiles.lima@hotmail.com>
2020-06-15 15:54:18 -0300

    listando registros cadastrados via o verbo Get

---
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -0,0 +1,33 @@
+package com.algaworks.osworks.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.osworks.domain.model.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("Pedro");
+		cliente1.setTelefone("32252222");
+		cliente1.setEmail("pedro@gmail.com");
+		
+		Cliente cliente2 = new Cliente();
+		
+		cliente2.setId(2L);
+		cliente2.setNome("Amanda");
+		cliente2.setTelefone("32255555");
+		cliente2.setEmail("amanda@gmail.com");
+		
+		
+		return Arrays.asList(cliente1, cliente2 );
+	}
+
+}
